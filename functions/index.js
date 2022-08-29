89008b57bedbbb5473980f9c4871df9bc4ec8749
developedbyssony <n_uck@naver.com>
2022-08-29 14:27:16 +0900

    make it better

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require("firebase-functions");
-const cors = require("cors")({origin: true});
+const cors = require("cors");
+const express = require("express");
 const request = require("request");
+const app = express();
+
+app.use(cors({origin: true}));
+
+
 exports.apicall = functions.https.onRequest((req, response) => {
   cors(req, response, () => {
     request("http://api.visitjeju.net/vsjApi/contents/searchList?apiKey=rwai4urhns253zsg&locale=kr", function(error, res, body) {
-      return response.send(res);
+      response.send(res);
     });
   });
 });
